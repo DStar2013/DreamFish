5342d4aada9a0483236d44f7ea9b204f4352758b
xjshuang <xjshuang@Ctrip.com>
2015-03-10 16:53:17 +0800

    add index

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 (function(){
 
-
+	$('#navMenu li').click(function(e) {
+		$('#sidebar .wrap-col').css('display','none');
+		$('#navMenu li').removeClass("active");
+		$(this).addClass("active");
+		document.getElementById($(this).attr("side")).style.display = '';
+		e.stopPropagation();
+	});
 
 
 
