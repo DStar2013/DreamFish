6669bc5650719fc28ef24f502e7f14af20d50bb2
xjshuang <xjshuang@Ctrip.com>
2015-08-14 20:49:02 +0800

    add 视觉差效果2

--- FunnyDemo/dstar-eyeDiff/js/main.js
+++ FunnyDemo/dstar-eyeDiff/js/main.js
@@ -22,8 +22,23 @@
 		});
 	})();
 	//第二类视觉差效果
-
-
+	(function() {
+		var boxH = $('div.content').height();
+		$('.article').find('div.content:eq(0)').addClass('content-focus')
+		//
+		$(window).scroll(function(e) {
+			var tmpH = $(window).scrollTop();
+			$("#PageBottom2 li:eq(" + parseInt(tmpH / boxH) + ")").addClass("focus").siblings().removeClass("focus");
+			$(".article>div.content:eq(" + parseInt(tmpH / boxH) + ")").addClass('content-focus').siblings().removeClass("content-focus");
+		});
+		//相应点击滚动
+		$('#PageBottom2 li').bind('click',function(){
+			$('body').animate({
+				scrollTop: $(this).index() * boxH
+			}, 2000);
+			return false;
+		});			
+	})();
 
 
 
