a6218e7d472deca7b92ba3a0bdb159e62dbfb940
xjshuang <xjshuang@Ctrip.com>
2015-08-14 17:56:54 +0800

    目录层级更改

--- FunnyDemo/dstar-eyeDiff/js/main.js
+++ FunnyDemo/dstar-eyeDiff/js/main.js
@@ -4,17 +4,17 @@
 		var boxH = $('.box').height();
 		var vIndex = [];
 		//
-		$('.slidecount li').each(function(){
+		$('#PageBottom1 li').each(function(){
 			vIndex.push($(this).index());
 		});
 
 		//响应window滚动
 		$(window).scroll(function(e) {
 			var tmpH = $(window).scrollTop();
-			$(".slidecount li:eq(" + vIndex[parseInt(tmpH / boxH)] + ")").addClass("focus").siblings("li").removeClass("focus");
+			$("#PageBottom1 li:eq(" + vIndex[parseInt(tmpH / boxH)] + ")").addClass("focus").siblings("li").removeClass("focus");
 		});
 		//响应点击滚动
-		$('.slidecount li').bind('click',function(){
+		$('#PageBottom1 li').bind('click',function(){
 			$('body').animate({
 				scrollTop: $(this).index() * boxH
 			}, 2000);
