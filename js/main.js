1eff28a4010da8f9d626db12646dc35e19625280
xjshuang <xjshuang@Ctrip.com>
2015-11-25 21:57:01 +0800

    add 头像旋转

--- js/main.js
+++ js/main.js
@@ -8,27 +8,65 @@
 		e.stopPropagation();
 	});
 
-
-	// $.mod.load('sideBar', '2.0', function() {
-	// 	//<a href="javascript:void(0);" class="bar-item bar-oa" onmouseover="OATips();" onmouseout="OAHide();"><i class="ico icon-oa"></i><span class="bar-title">${oa}</span></a>
-	// 	var sidebar = $(document).regMod('sideBar', '2.0', {
-	// 		HTML: '<div class="xui-fixbar"><a href="javascript:alert(\'酒店审批\');" class="bar-item bar-oa-hotel"><i class="xuico-hotel-white2"></i><span class="bar-title">${hotelOA}</span></a><a href="${suggestionURL}" target="_blank" class="bar-item bar-feedback"><i class="xuico-feedback"></i><span class="bar-title">${suggestion}</span></a><a href="${liveChatURL}" target="_blank" class="bar-item bar-olservice" style="display:none"><i class="xuico-olservice"></i><span class="bar-title">${liveChat}</span></a><a href="#" class="bar-item bar-totop"><i class="xuico-totop"></i></a></div>',
-	// 		url: {
-	// 			suggestionURL: 'http://accounts.ctrip.com/MyCtrip/Community/CommunityAdvice.aspx?productType=15',
-	// 			liveChatURL: 'http://livechat.ctrip.com/livechat/Login.aspx?GroupCode=slzzpt'
-	// 		},
-	// 		title: {
-	// 			backTop: '回到顶部',
-	// 			suggestion: '意见反馈',
-	// 			liveChat: '在线客服',
-	// 			hotelOA: '酒店OA审批'
-	// 		},
-	// 		threshold_px: 100
-	// 	});
-	// });
-
-	(function(){
+	/*
+	var li = $('.gallery').find('li');
 		
-	})();
+	li.each(function(i){
+		var t = $(this),
+			img = t.find('img'),
+			src = img.attr('src'),
+			width = li.width(),
+			height = li.height();
+			
+		img.hide().after($('<div />').attr('id', 'holder'+i).addClass('holder'));
+
+		var r = Raphael('holder'+i, width*2, height*2),
+			rimg = r.image(src, width/2, height/2, width, height);
+		
+		rimg.hover(function(event) {
+		    this.animate({
+				scale: 2,
+				rotation : 360
+			}, 1200, 'elastic');
+		}, function (event) {
+		    this.animate({
+				scale: 1,
+				rotation : 0
+			}, 1200, 'elastic');
+		});
+		
+	});
+*/
+	/*
+	$('#ImgGallery').find('a').each(function(i) {
+		var t = $(this),
+			img = t.find('img'),
+			src = img.attr('src'),
+			width = 500,
+			height = 300;
+
+		img.hide().after($('<div />').attr('id', 'holder'+i).addClass('holder'));
+		// img.attr('id','holder'+i).addClass('holder');
+
+		var r = Raphael('holder'+i, width*2, height*2),
+			rimg = r.image(src, width/2, height/2, width, height);
+
+		rimg.hover(function(event) {
+		    this.animate({
+				scale: 2,
+				rotation : 360
+			}, 1200, 'elastic');
+		}, function (event) {
+		    this.animate({
+				scale: 1,
+				rotation : 0
+			}, 1200, 'elastic');
+		});
+
+
+
+	});
+*/
+
 
 })();
