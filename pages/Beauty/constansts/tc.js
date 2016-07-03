e0b64b17765016e40ee77472960f02e639e07206
xjshuang <531388612@qq.com>
2016-07-03 21:54:36 +0800

    add constant

---
+++ pages/Beauty/constansts/tc.js
@@ -0,0 +1,20 @@
+/**
+ * Created by dream on 16/7/3.
+ */
+
+(function (w) {
+    if (w.TC) return;
+
+    w.TC = {
+        FirstMeetTime: "2016/06/01",
+        YourLoveIdol: "林俊杰"
+
+
+    }
+
+
+})(window);
+
+
+
+
