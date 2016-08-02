f980b60a17261bf53e654b8b5cdf71d426444f96
xjshuang <531388612@qq.com>
2016-08-02 15:18:23 +0800

    add sth

--- FunnyDemo/forMyLove/js/Love/index.js
+++ FunnyDemo/forMyLove/js/Love/index.js
@@ -6,25 +6,25 @@
             ts = new Date(2012, 0, 1),
             newYear = true;
 
-        if((new Date()) > ts){
+        if ((new Date()) > ts) {
             // The new year is here! Count towards something else.
             // Notice the *1000 at the end - time must be in milliseconds
-            ts = (new Date()).getTime() + 10*24*60*60*1000;
+            ts = (new Date()).getTime() + 10 * 24 * 60 * 60 * 1000;
             newYear = false;
         }
 
         $('#countdown').countdown({
-            timestamp	: ts,
-            callback	: function(days, hours, minutes, seconds){
+            timestamp: ts,
+            callback: function (days, hours, minutes, seconds) {
 
                 var message = "";
 
-                message += days + " day" + ( days==1 ? '':'s' ) + ", ";
-                message += hours + " hour" + ( hours==1 ? '':'s' ) + ", ";
-                message += minutes + " minute" + ( minutes==1 ? '':'s' ) + " and ";
-                message += seconds + " second" + ( seconds==1 ? '':'s' ) + " <br />";
+                message += days + " day" + ( days == 1 ? '' : 's' ) + ", ";
+                message += hours + " hour" + ( hours == 1 ? '' : 's' ) + ", ";
+                message += minutes + " minute" + ( minutes == 1 ? '' : 's' ) + " and ";
+                message += seconds + " second" + ( seconds == 1 ? '' : 's' ) + " <br />";
 
-                if(newYear){
+                if (newYear) {
                     message += "left until the new year!";
                 }
                 else {
@@ -37,7 +37,7 @@
     }
 
 
-    $(document).ready(function(){
+    $(document).ready(function () {
         //
         $("#top").lettering();
         $("#bottom").lettering();
