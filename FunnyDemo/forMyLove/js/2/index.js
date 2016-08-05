b4edb2ee45955df85c96ded561d77947ea5d9ae7
xjshuang <531388612@qq.com>
2016-08-05 16:08:31 +0800

    add animate

--- FunnyDemo/forMyLove/js/2/index.js
+++ FunnyDemo/forMyLove/js/2/index.js
@@ -5,36 +5,63 @@
 
 (function ($) {
 
-
-    function textShowInit(){
+    function textShowInit() {
         var pList = $('#st-content').find('p');
+        //js dom list 转化为数组对象
+        var pArr = Array.prototype.slice.call(pList);
+        var animateListFun = [];
+        //动画顺序
+        var animateIndex = 0;
 
-        var animateListFun = []
-
-        pList.each(function(i, obj) {
-            switch (i % 3) {
-                case 0:
-                    $(obj).animate(function(){
-
-                    })
-                    break;
-                case 1:
-                    break;
-                case 2:
-                    break;
-                default:
-                    break;
+        var animateFun = function (pArr, successCallBack) {
+            if (!pArr || pArr.length == 0) return;
+            var currObj = $(pArr[0]);
+            pArr.shift();
+            //循环动画
+            if (pArr.length > 0) {
+                animateIndex == 3 ? animateIndex = 1 : animateIndex++;
+                switch (animateIndex) {
+                    case 1:
+                        currObj.show(3000, function(){
+                            animateFun(pArr);
+                        });
+                        break;
+                    case 2:
+                        currObj.fadeIn(3000, function(){
+                            animateFun(pArr);
+                        });
+                        break;
+                    case 3:
+                        currObj.slideDown(3000,function(){
+                            animateFun(pArr);
+                        });
+                        break;
+                    default:
+                        currObj.show(5000, function(){
+                            animateFun(pArr);
+                        });
+                        break;
+                }
+            } else {
+                currObj.show(3000, function(){
+                    animateFun(pArr);
+                });
             }
 
+        }
 
-
-        });
-
+        animateFun(pArr);
 
     }
 
+    //Ev init
+    function EventInit() {
+        $('#p-next').bind('click', function () {
+            alert("CC")
+        });
+    }
 
-    $(document).ready(function(){
+    $(document).ready(function () {
 
         //
         textShowInit();
